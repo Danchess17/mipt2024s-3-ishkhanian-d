65d15e6a99edca849520475d7d9b0ab319a5492d
Danchess17 <ishkhanian.da@phystech.edu>
2024-02-14 22:17:43 +0300

    changed stack_with_array.hpp

--- stack_with_array.hpp
+++ stack_with_array.hpp
@@ -2,73 +2,99 @@
 
 template <typename T>
 class StackWithArray {
-    private:
-        T* stack;
-        int capacity;  
-        int length;
+ private:
+  T* stack;
+  int capacity;
+  int length;
 
-    public:
-        StackWithArray(int cap = 8) : length(0), capacity(cap) {
-            stack = new T[cap];
-        }
+ public:
+  StackWithArray(int cap = 8) : length(0), capacity(cap) { stack = new T[cap]; }
 
-        ~StackWithArray() {
-            delete [] stack;
-        }
+  StackWithArray(const StackWithArray<T>& s)
+      : length(s.length), capacity(s.capacity) {
+    stack = new T[capacity];
+    for (int i = 0; i < length; ++i) {
+      stack[i] = s.stack[i];
+    }
+  }
 
-        bool empty() const {
-            return (length == 0);
-        }
+  ~StackWithArray() { delete[] stack; }
 
-        void push(const T& elem) {
-            if (length == capacity) {
-                capacity *= 2;
-                T* newstack = new T[capacity];
-                for (int i = 0; i < length; ++i) {
-                    newstack[i] = stack[i];
-                }
-                delete [] stack;
-                stack = newstack;
-            }
-            stack[length] = elem;
-            length++;
-        }
+  StackWithArray& operator=(const StackWithArray<T>& s) {
+    if (this != &s) {
+      delete[] stack;
+      length = s.length;
+      capacity = s.capacity;
+      stack = new T[capacity];
+      for (int i = 0; i < length; ++i) {
+        stack[i] = s.stack[i];
+      }
+    }
+    return *this;
+  }
 
-        T top() const {
-            if (length == 0) {
-                std::cout << "Error: taking top element from empty stack..." << std::endl;
-                exit(1);
-            }
-            return stack[length - 1];
-        }
+  StackWithArray(StackWithArray<T>&& s) noexcept
+      : length(0), capacity(0), stack(nullptr) {
+    std::swap(length, s.length);
+    std::swap(capacity, s.capacity);
+    std::swap(stack, s.stack);
+  }
 
-        T pop() {
-            if (length == 0) {
-                std::cout << "Error: popping top element from empty stack..." << std::endl;
-                exit(1);
-            }
-            T temp = stack[length - 1]; 
-            length--; 
-            if (length < capacity / 4) { 
-                capacity /= 2; 
-                T* newstack = new T[capacity];
-                for (int i = 0; i < length; ++i) { 
-                    newstack[i] = stack[i]; 
-                }
-                delete [] stack;
-                stack = newstack;
-            }
-            return temp;
-        }
+  StackWithArray& operator=(StackWithArray<T>&& s) noexcept {
+    std::swap(length, s.length);
+    std::swap(capacity, s.capacity);
+    std::swap(stack, s.stack);
+    return *this;
+  }
 
-        int size() const {
-            return length;
-        }
+  void push(const T& elem) {
+    if (length == capacity) {
+      capacity *= 2;
+      T* newstack = new T[capacity];
+      for (int i = 0; i < length; ++i) {
+        newstack[i] = stack[i];
+      }
+      delete[] stack;
+      stack = newstack;
+    }
+    stack[length] = elem;
+    length++;
+  }
 
-        void display() const {
-            for (int i = 0; i < length; ++i) {
-                std::cout << stack[length - i - 1] << " -> ";
-            }
-            std::cout << std::endl;
-        };
+  void pop() noexcept {
+    if (length == 0) {
+      std::cout << "Error: popping top element from empty stack..."
+                << std::endl;
+      exit(1);
+    }
+    length--;
+    if (length < capacity / 4) {
+      capacity /= 2;
+      T* newstack = new T[capacity];
+      for (int i = 0; i < length; ++i) {
+        newstack[i] = stack[i];
+      }
+      delete[] stack;
+      stack = newstack;
+    }
+  }
+
+  bool empty() const noexcept { return (length == 0); }
+
+  const T& top() const {
+    if (length == 0) {
+      std::cout << "Error: taking top element from empty stack..." << std::endl;
+      exit(1);
+    }
+    return stack[length - 1];
+  }
+
+  int size() const noexcept { return length; }
+
+  void display() const {
+    for (int i = 0; i < length; ++i) {
+      std::cout << stack[length - i - 1] << " ";
+    }
+    std::cout << std::endl;
+  };
 };
