48ea345939b3df71956bb4f8ae9f594ec7103bf6
Danchess17 <ishkhanian.da@phystech.edu>
2024-05-24 17:42:13 +0300

    argc argv changes

--- prj.cw/sift3d/affine-generator.cpp
+++ prj.cw/sift3d/affine-generator.cpp
@@ -4,36 +4,37 @@
 #include <sys/stat.h>
 #include <string>
 
-int main() {
-    // std::cout << "Please, enter your operational system\n";
-    // std::cout << "L - for Linux, W - for Windows\n";
-    char os = 'L'; 
-    // std::cin >> os;
-    std::cout << "Please, enter type of affine transformation: \n";
-    std::cout << "T - for translation, R - for rotation\n";
-    char affine_mode = 'T';
-    std::cin >> affine_mode;
+int main(int argc, char** argv) {
+    if (argc == 1) {
+        std::cout << "Error!\nYou should enter type of affine transformation:\n";
+        std::cout << "T - for translation, R - for rotation\n";
+        return 0;
+    }
+    char affine_mode = argv[1][0];
+    std::cout << "Generating object...\n";
     char buffer[1024];
     int dataset_size;
     if (affine_mode == 'T') {
+        if (argc != 5) {
+            std::cout << "Error!\nTranslation parameters (x, y, z) are not passed!\nYou should enter x, y, z\n";
+            return 0;
+        }
         std::filesystem::create_directory("translations");
-        //mkdir("translations", 0777); // not sure it will work for Windows
-        int x, y, z;
-        std::cout << "Enter x, y, z:\n";
-        std::cin >> x >> y >> z;
+        int x = atoi(argv[2]);
+        int y = atoi(argv[3]);
+        int z = atoi(argv[4]);
 
         snprintf(buffer, sizeof(buffer), "python3 ../prj.cw/sift3d/translator.py %d %d %d", x, y, z);
         system(buffer);
     }
     else if (affine_mode == 'R') {
-        std::filesystem::create_directory("translations");
-        // mkdir("rotations", 0777);
-        // std::cout << "Enter amount of NIFTI files you want to generate:\n";
-        // std::cin >> dataset_size;
+        if (argc != 3) {
+            std::cout << "Error!\nX axis degree rotation parameter is not passed!\nYou should enter deg\n";
+            return 0;
+        }
+        std::filesystem::create_directory("rotations");
+        std::cout << deg;
 
-        int deg;
-        std::cout << "Enter degree for x axis rotation:\n";
-        std::cin >> deg;
         snprintf(buffer, sizeof(buffer), "python3 ../prj.cw/sift3d/rotator.py %d", deg);
         system(buffer);
     }
